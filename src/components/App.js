6f672cfff2f9fe154b8cfb4456f5592b7521f84a
github username <github email address>
2018-01-11 10:31:52 -0500

    initial changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-import '../App.css';
-import Nav from './Nav'
+import React, { Component } from "react";
+import "../App.css";
+import Nav from "./Nav";
+import porkers from "../porkers_data";
 
 class App extends Component {
+  constructor() {
+    super();
+  }
   render() {
     return (
       <div className="App">
-          < Nav />
-
+        <Nav porkers={porkers} />
       </div>
-    )
+    );
   }
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,16 @@
-import piggy from '../porco.png';
-import React from 'react';
+import piggy from "../porco.png";
+import React from "react";
 
-const Nav = () => {
+// const jpgs = require.context("../src/hog-imgs", true, /\.jpg$/);
+
+const Nav = props => {
+  // function renderPics(props) {
+  //   props.forEach(porker => {
+  //     if (props.name
+  //   });
+  // }
+  // let thing = "../hog-imgs/cherub.jpg"
+
   return (
     <div className="navWrapper">
       <span className="headerText">Hogwarts</span>
@@ -10,7 +19,16 @@ const Nav = () => {
       </div>
       <span className="normalText">A React App for County Fair Hog Fans</span>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
+// {jpgs.map(jpg => console.log)}
+// <div className="hogTile">
+//   {props.porkers.map((pork, i) => (
+//     <div className="pork">
+//       <p key={i}>{pork.name}</p>
+//       <img alt="piggysmalls" />
+//     </div>
+//   ))}
+// </div>
