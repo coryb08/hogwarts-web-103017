08be954000526f6b09c6733c45f33c35983e0ddd
github username <github email address>
2018-01-11 11:07:01 -0500

    pics show

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {porkers.map(pork => {
-          <Nav img={require(`../hog-imgs/mudblood.jpg`)} pork={pork} />;
-        })}
+        <Nav porkers={porkers} />
       </div>
     );
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,6 @@
 import piggy from "../porco.png";
 import React from "react";
 
-// const jpgs = require.context("../src/hog-imgs", true, /\.jpg$/);
 
 const Nav = props => {
   return (
@@ -12,16 +11,15 @@ const Nav = props => {
       </div>
       <span className="normalText">A React App for County Fair Hog Fans</span>
       <div className="hogTile">
-        <div className="pork">
-          <p key={props.pork.name}>{props.pork.name}</p>
-          <img src={props.img} />
-        </div>
+        {props.porkers.map(pork => (
+          <div className="pork">
+            <p key={pork.name}>{pork.name}</p>
+            <img src={require(`../hog-imgs/${pork.name.toLowerCase()}.jpg`)} />
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
 export default Nav;
-// {jpgs.map(jpg => console.log)}
-
-// <img src={require("../hog-imgs/mudblood.jpg")} />
